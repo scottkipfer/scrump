3ba39edbaecde1cbbdbf60a3e61a06e894918f5b
Scott Kipfer <scottkipfer@gmail.com>
2018-10-11 22:36:41 -0400

    updated git ignore

--- server/core/db.js
+++ server/core/db.js
@@ -1,18 +1,18 @@
 const mongoose = require('mongoose');
 const config = require('config');
 const mongoLocation = 'mongodb://database/test';
-const mongoOptions = {useNewUrlParser: true};
+const mongoOptions = { useNewUrlParser: true };
 
 mongoose.Promise = Promise;
 mongoose.connect(mongoLocation, mongoOptions).catch((err) => {
-   console.warn(err);
+  console.warn(err);
 });
 
 process.on('SIGINT', () => {
-   mongoose.connection.close(() => {
-      console.log('Mongoose default connection disconnection');
-      process.exit(0);
-   });
+  mongoose.connection.close(() => {
+    console.log('Mongoose default connection disconnection');
+    process.exit(0);
+  });
 });
 
 require('../lib/task/task.model.js');
