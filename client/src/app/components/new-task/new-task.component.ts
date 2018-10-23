0584fd531dccfa6b74174f89feacd10882f5231d
faktorsmak <rob.zakrzewski@gmail.com>
2018-10-23 14:29:45 -0400

    populate the board for the new task based on where you are at

--- client/src/app/components/new-task/new-task.component.ts
+++ client/src/app/components/new-task/new-task.component.ts
@@ -1,8 +1,10 @@
 import { Component, OnInit } from '@angular/core';
 import { NgbActiveModal } from '@ng-bootstrap/ng-bootstrap';
 import { Store} from '@ngrx/store';
+import { tap, take } from 'rxjs/operators'
 import * as fromStore from '../../store';
 import {CreateTaskModel} from '../../models';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-new-task',
@@ -11,16 +13,29 @@ import {CreateTaskModel} from '../../models';
 })
 export class NewTaskComponent implements OnInit {
   task: CreateTaskModel = new CreateTaskModel();
+  currentView$: Observable<string>;
+  defaultLocation: string;
 
 
   initTask() {
     this.task.task.summary = '';
     this.task.task.requirementUrl = null;
-    this.task.board = 'backlog';
+    this.task.board = null;
   }
 
   ngOnInit() {
     this.initTask();
+    this.store.select(fromStore.getCurrentView).pipe(
+      take(1),
+      tap(view => {
+        switch (view) {
+          case 'current':
+            this.task.board = 'sprint'
+            break;
+          default: this.task.board = view;
+        }
+      })
+    ).subscribe();
   }
 
   onClose() {
